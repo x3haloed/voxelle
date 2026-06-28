c1f6595b222fba74d53944c760c72f80815e3964
Chad Kirby <x3haloed@gmail.com>
2026-06-27 23:38:52 -0700

    chore: remove legacy web stack

--- apps/web/src/voxelle/rfc/hash.ts
+++
@@ -1,4 +0,0 @@
-export async function sha256(bytes: Uint8Array): Promise<Uint8Array> {
-  const digest = await crypto.subtle.digest('SHA-256', bytes as unknown as BufferSource)
-  return new Uint8Array(digest)
-}
